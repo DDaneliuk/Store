0f8806a5d5eb8f8467a929c2740a24a3a47688b0
ddanelyuk <ddanelyuk@student.ucode.world>
2020-12-19 19:28:37 +0200

    Optimation for all devices

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
+var dataproduct;
 fetch("./products.json")
     .then(function(resp){
         return resp.json();
     })
-    .then(function(data){
-        showPageData(data);
-    })
-    var pageCards = 6;
-function showPageData(data, wrapper, pageBlocks, pages){
-    var countElement = data.length;
+    .then(function(data){ 
+        console.log(data);
+            // Filter(data)
+            DisplayList(data, productBlock, rows, currentPage);
+            SetupPagination(data, pagination_element, rows);
+            return data;
+    });
+        
+const productBlock = document.getElementById('product_block');
+const pagination_element = document.getElementById('pagination');
 
-    var currentPage = 1;
-    console.log(countElement);
-    
+let currentPage = 1;
+let rows = 6;
 
-    data.forEach(element => {
+function DisplayList (data, productBlock, rows, currentPage) {
+	productBlock.innerHTML = "";
+	currentPage--;
+
+	let start = rows * currentPage;
+	let end = start + rows;
+    let paginatedItems = data.slice(start, end);
+    console.log(paginatedItems)
+
+    paginatedItems.forEach(element => {
         const newProduct = document.createElement('div');    //create product div
         newProduct.className = 'product';
         product_block.appendChild(newProduct);
@@ -36,13 +49,65 @@ function showPageData(data, wrapper, pageBlocks, pages){
         newProductName.innerHTML = element.name;
         newProductInfo.appendChild(newProductName);
 
+        const newProductTags = document.createElement("div"); // add block with tags
+        newProductTags.setAttribute("class", "productsTags")
+        newProductInfo.appendChild(newProductTags);
+
+        const newProductSize = document.createElement("p"); //add size of product
+        newProductSize.innerHTML = "size:"+ element.size;
+        newProductTags.appendChild(newProductSize);
+        const newProductSizeAttId = document.createAttribute('id'); // add id to size for filters
+        newProductSize.setAttribute("id","productSize")
+        const newProductSizeAttClass = document.createAttribute('class');// add class to size for tags
+        newProductSize.setAttribute("class","productTag");
+        newProductSize.setAttribute("value", element.size);// add class to color for tags
+        
+
+        const newProductColor = document.createElement("p"); //add color of product
+        newProductColor.innerHTML = "color:"+ element.color;
+        newProductTags.appendChild(newProductColor); 
+        newProductColor.setAttribute("id","productColor")// add id to color for filters  
+        newProductColor.setAttribute("class","productTag");
+        newProductColor.setAttribute("value","productTag");// add class to color for tags
+
         const newProductPrice = document.createElement("p"); //add price of product
         newProductPrice.innerHTML = element.price;
         newProductInfo.appendChild(newProductPrice);
+        newProductPrice.setAttribute("id","productPrice");
 
         const newButton = document.createElement("button");
         newButton.className = 'product_buy';
         newButton.textContent = 'Order now';
         newProductInfo.appendChild(newButton);
-    }); 
+    });	    
+}
+
+function SetupPagination (data, productBlock, rows) {
+	productBlock.innerHTML = "";
+
+	let page_count = Math.ceil(data.length / rows);
+	for (let i = 1; i < page_count + 1; i++) {
+		let btn = PaginationButton(i, data);
+		productBlock.appendChild(btn);
+	}
+}
+function PaginationButton (page, data) {
+	let button = document.createElement('button');
+	button.innerText = page;
+
+    if (currentPage == page) button.classList.add('activePage');
+    if (currentPage != page) button.classList.add('buttonPage');
+
+	button.addEventListener('click', function () {
+		currentPage = page;
+		DisplayList(data, productBlock, rows, currentPage);
+
+		let current_btn = document.querySelector('.activePage');
+        current_btn.classList.remove('activePage');
+        current_btn.classList.add('buttonPage');
+
+		button.classList.add('activePage');
+	});
+
+	return button;
 }
---
+++ js/test.js
@@ -0,0 +1,17 @@
+// Filters 
+// function Filter(data){
+//     var allColors = document.querySelectorAll('div.colors input[type=checkbox]');
+//     console.log(allColors);
+//     var allProducts = data;
+//     console.log(allProducts);
+//     var checed={};
+//     getChecked('color');
+//     console.log(getChecked);
+     
+   
+// }
+// function test(){
+//     getChecked('color');
+//     console.log(getChecked);
+// }
+
